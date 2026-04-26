cfca22688eecc044493e6cc4ae9668d0080d7650
dpqgvryhph-pixel <dpqgvryhph@privaterelay.appleid.com>
2026-04-26 09:22:57 +0200

    fix: next.config.js előkészítése Cloudflare Pages deployhoz

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Cloudflare Pages kompatibilitáshoz szükséges
+  // A @cloudflare/next-on-pages adapter kezeli a buildet
+};
+
 module.exports = nextConfig;
